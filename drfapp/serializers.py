b74c7ef5ff38ff5b44499191a8546c844cbc3ecb
JoMingyu <mingyu.planb@gmail.com>
2019-08-20 23:24:21 +0900

    serializer를 만들었다. 하라길래 했는데 대충 django admin 계정과 관련된게 아닐까?

---
+++ drfapp/serializers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User, Group
+from rest_framework.serializers import HyperlinkedModelSerializer
+
+
+class UserSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
