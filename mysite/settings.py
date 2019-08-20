efd0f3e107bdd2f827ac534c49ac680ebc2e2017
JoMingyu <mingyu.planb@gmail.com>
2019-08-20 23:20:33 +0900

    지운 기존 앱 import하는 거 있어서 고침. 이거 좀 자동으로 못하나

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
